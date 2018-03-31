dec089f931660bf3403b1c45834a51bb2f49e563
shalini kumari <shalinikumari2598@gmail.com>
2018-03-31 10:07:25 +0530

    Add queries to playground

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,41 @@
+const{ObjectID}= require('mongodb');
+
+const{mongoose} = require('./../server/db/mongoose');
+const{Todo}= require('./../server/models/todo');
+const{User} = require('./../server/models/user');
+
+//var id = '5abe8c08d97f4c29748271bb';
+
+// if(!ObjectID.isValid(id)){
+//   console.log('Id not valid');
+// }
+
+// Todo.find({
+//   _id : id
+// }).then((todos)=>{
+//   console.log('Todos', todos);
+// });
+//
+// Todo.findOne({
+//   _id : id
+// }).then((todo)=>{
+//   console.log('Todo', todo);
+// });
+
+// Todo.findById(id).then((todo)=>{
+//    if(!todo){
+//      return console.log('Id not found');
+//    }
+//   console.log('Todo By Id', todo);
+// }).catch((e)=>console.log(e));
+
+
+User.findById('5aa7e7e481ca362ed85d6459').then((user)=>{
+  if(!user){
+    return console.log('Unable to find user');
+  }
+
+  console.log(JSON.stringify(user , undefined , 2));
+}),(e)=>{
+  console.log(e);
+}
