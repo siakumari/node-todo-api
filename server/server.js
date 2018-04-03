7951f23c764dc1460af7d2591fad532c10a07a30
shalini kumari <shalinikumari2598@gmail.com>
2018-04-03 09:07:19 +0530

    Add test for POST/users/login

--- server/server.js
+++ server/server.js
@@ -117,7 +117,7 @@ app.get('/users/me',authenticate ,(req ,res)=>{
 
 });
 
-// POST/users/login{email , password}
+
 app.post('/users/login',(req , res)=>{
   var body = _.pick(req.body,['email','password']);
    User.findByCredentials(body.email,body.password).then((user)=>{
